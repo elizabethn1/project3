0dba5b98f672f813f62319bc91b1ee19800f4e60
elizabethn1 <112528779+elizabethn1@users.noreply.github.com>
2022-11-20 14:41:12 -0600

    tried fixing my map

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-var map;
+/*var map;
 function initMap(){
   map = new google.maps.Map(document.getElementById('mapcanvas'), {
   center: {lat: 35.65926, lng: 139.70127879514445},
   zoom: 15
   });
-}
+}*/
 
-window.initMap=initMap;
-
-/*function init(){
+function initMap(){
 	var m=document.getElementById('mapcanvas');
 	var myLocation=new google.maps.LatLng(35.65926, 139.70127879514445);
 	var mapOptions={
 		center: myLocation,
 		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
 	};
 
 	var myMap=new google.maps.Map(m, mapOptions);
-}*/
+
+
+	//alert('it works');
+}
+
+//google.maps.event.addDOMListener(window, 'load', initMap);
+
+window.initMap=initMap;
 
 
