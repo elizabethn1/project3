3f7f6dd1245b6530f5c1dd523b11ad5912a56199
elizabethn1 <112528779+elizabethn1@users.noreply.github.com>
2022-11-19 20:25:32 -0600

    trying to get my map to work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+function initMap(){
+  map = new google.maps.Map(document.getElementById('mapcanvas'), {
   center: {lat: 35.65926, lng: 139.70127879514445},
   zoom: 15
   });
 }
+
+window.initMap=initMap;
+
+/*function init(){
+	var m=document.getElementById('mapcanvas');
+	var myLocation=new google.maps.LatLng(35.65926, 139.70127879514445);
+	var mapOptions={
+		center: myLocation,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap=new google.maps.Map(m, mapOptions);
+}*/
+
+
